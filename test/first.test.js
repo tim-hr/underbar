b623c260231d54d3e79bca23779f8161c9d761ff
ksato24 <kyosuke.satou24@gmail.com>
2018-01-27 11:24:01 +0900

    add tests of first and last functions

--- test/first.test.js
+++ test/first.test.js
@@ -12,4 +12,28 @@ describe('first()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.first(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+
+  it('returns the whole array if you ask for all elements', () => {
+  	expect(_.first(['a', 'b', 'c'], 3)).toEqual(['a', 'b', 'c']);
+  });
+
+  it('returns an empty array if you pass zero', () => {
+  	expect(_.first(['a', 'b', 'c'], 0)).toEqual([]);
+  });
+
+  it('returns the first several elements of an array if you pass a negative number', () => {
+  	expect(_.first(['a', 'b', 'c'], -1)).toEqual(['a', 'b']);
+  });
+
+  it('returns the first element of an array if you pass a negative number', () => {
+    expect(_.first(['a', 'b', 'c'], -2)).toEqual(['a']);
+  });
+
+  it('returns an empty array if you pass a negative number of which absolute value is equal to length of the array', () => {
+  	expect(_.first(['a', 'b', 'c'], -3)).toEqual([]);
+  });
+
+  it('returns an empty array if you pass a negative number of which absolute value is more than length of the array', () => {
+  	expect(_.first(['a', 'b', 'c'], -5)).toEqual([]);
+  });
 });
