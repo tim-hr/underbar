b3f0eebbe6a7d44536970c4ad45516a603291c37
ksato24 <kyosuke.satou24@gmail.com>
2018-01-30 20:27:06 +0900

    add tests of every, filter, reject, some, and uniq functions

--- test/every.test.js
+++ test/every.test.js
@@ -11,6 +11,111 @@ describe('every()', () => {
       const nums = [1, 3, 5, 6, 7];
       expect(_.every(nums, num => num % 2 === 1)).toBe(false);
     });
+  });
 
+  describe('processing an array-like object of which values are numbers', () => {
+    it('returns true if all numbers in an array-like object are odd', () => {
+      const nums = {
+        length: 4,
+        0: 1,
+        1: 3,
+        2: 5,
+        3: 7
+      };
+      expect(_.every(nums, num => num % 2 === 1)).toBe(true);
+    });
+
+    it('returns false if not all numbers in an array-like object are odd', () => {
+      const nums = {
+        length: 5,
+        0: 1,
+        1: 3,
+        2: 5,
+        3: 6,
+        4: 7
+      };
+      expect(_.every(nums, num => num % 2 === 1)).toBe(false);
+    });
+  });
+
+  describe('processing an object of which values are numbers', () => {
+    it('returns true if all numbers in an object are odd', () => {
+      const nums = {
+        'a': 1,
+        'b': 3,
+        'c': 5,
+        'd': 7
+      };
+      expect(_.every(nums, num => num % 2 === 1)).toBe(true);
+    });
+
+    it('returns false if not all numbers in an object are odd', () => {
+      const nums = {
+        'a': 1,
+        'b': 3,
+        'c': 5,
+        'd': 6,
+        'e': 7
+      };
+      expect(_.every(nums, num => num % 2 === 1)).toBe(false);
+    });
+  });
+
+  describe('processing an empty objects', () => {
+    it('returns true if an array is empty', () => {
+      expect(_.every([], num => num % 2 === 1)).toBe(true);
+    });
+
+    it('returns true if an array-like object is empty', () => {
+      expect(_.every({ length: 0 }, num => num % 2 === 1)).toBe(true);
+    });
+
+    it('returns true if an object is empty', () => {
+      expect(_.every({}, num => num % 2 === 1)).toBe(true);
+    });
+  });
+
+  describe('a callback function returns truthy values', () => {
+    it('returns true if a callback function returns an empty object', () => {
+      expect(_.every([1, 3, 5], () => ({}))).toBe(true);
+    });
+
+    it('returns true if a callback function returns an empty array', () => {
+      expect(_.every([1, 3, 5], () => [])).toBe(true);
+    });
+
+    it('returns true if a callback function returns a number', () => {
+      expect(_.every([1, 3, 5], () => 42)).toBe(true);
+    });
+
+    it('returns true if a callback function returns a non empty string', () => {
+      expect(_.every([1, 3, 5], () => 'foo')).toBe(true);
+    });
+
+    it('returns true if a callback function returns some object', () => {
+      expect(_.every([1, 3, 5], () => new Date())).toBe(true);
+    });
+  })
+
+  describe('a callback function returns falsy values', () => {
+    it('returns false if a callback function returns null', () => {
+      expect(_.every([1, 3, 5], () => null)).toBe(false);
+    });
+
+    it('returns false if a callback function returns undefined', () => {
+      expect(_.every([1, 3, 5], () => undefined)).toBe(false);
+    });
+
+    it('returns false if a callback function returns 0', () => {
+      expect(_.every([1, 3, 5], () => 0)).toBe(false);
+    });
+
+    it('returns false if a callback function returns NaN', () => {
+      expect(_.every([1, 3, 5], () => NaN)).toBe(false);
+    });
+
+    it('returns false if a callback function returns empty string', () => {
+      expect(_.every([1, 3, 5], () => '')).toBe(false);
+    });
   });
 });
--- test/reject.test.js
+++ test/reject.test.js
@@ -18,4 +18,67 @@ describe('reject()', () => {
     const orderItems = _.reject(order, (value) => value === null);
     expect(orderItems).toEqual(['burger', 'ketchup', 'cookie']);
   });
+
+  it('rejects odd numbers from an array-like object', () => {
+    const nums = {
+      length: 4,
+      0: 2,
+      1: 4,
+      2: 5,
+      3: 6
+    };
+    expect(_.reject(nums, num => num % 2 === 1)).toEqual([2, 4, 6]);
+  });
+
+  it('returns an empty array if an empty array is given', () => {
+    expect(_.reject([], num => num % 2 === 1)).toEqual([]);
+  });
+
+  it('returns an empty array if an empty array-like object is given', () => {
+    expect(_.reject({ length:0 }, num => num % 2 === 1)).toEqual([]);
+  });
+
+  it('returns an empty array if an empty object is given', () => {
+    expect(_.reject({}, num => num % 2 === 1)).toEqual([]);
+  });
+
+  it('returns an empty array if a callback always returns an empty object', () => {
+    expect(_.reject([1, 2, 3], () => ({}))).toEqual([]);
+  });
+
+  it('returns an empty array if a callback always returns an empty array', () => {
+    expect(_.reject([1, 2, 3], () => [])).toEqual([]);
+  });
+
+  it('returns an empty array if a callback always returns a number', () => {
+    expect(_.reject([1, 2, 3], () => 42)).toEqual([]);
+  });
+
+  it('returns an empty array if a callback always returns a string', () => {
+    expect(_.reject([1, 2, 3], () => 'foo')).toEqual([]);
+  });
+
+  it('returns an empty array if a callback always returns some object', () => {
+    expect(_.reject([1, 2, 3], () => new Date())).toEqual([]);
+  });
+
+  it('returns all elements if a callback always returns null', () => {
+    expect(_.reject([1, 2, 3], () => null)).toEqual([1, 2, 3]);
+  });
+
+  it('returns all elements if a callback always returns undefined', () => {
+    expect(_.reject([1, 2, 3], () => undefined)).toEqual([1, 2, 3]);
+  });
+
+  it('returns all elements if a callback always returns 0', () => {
+    expect(_.reject([1, 2, 3], () => 0)).toEqual([1, 2, 3]);
+  });
+
+  it('returns all elements if a callback always returns NaN', () => {
+    expect(_.reject([1, 2, 3], () => NaN)).toEqual([1, 2, 3]);
+  });
+
+  it('returns all elements if a callback always returns an empty string', () => {
+    expect(_.reject([1, 2, 3], () => null)).toEqual([1, 2, 3]);
+  });
 });
